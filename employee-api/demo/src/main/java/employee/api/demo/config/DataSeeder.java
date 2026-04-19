f7a5b27270c9a2068863b5f3d139202a81072da9
gischellatte <40295535+gischellatte@users.noreply.github.com>
2026-04-19 23:03:15 +1000

    Update DataSeeder.java

--- employee-api/demo/src/main/java/employee/api/demo/config/DataSeeder.java
+++ employee-api/demo/src/main/java/employee/api/demo/config/DataSeeder.java
@@ -20,12 +20,8 @@ public class DataSeeder implements CommandLineRunner{
     }
 
     @Override 
-     //you can save in repo under this method too
-     //employeeRepository.save(empGenerator);
     public void run(String... args) throws Exception {
         if(this.employeeRepository.count()==0){
-            //Create an employee object 
-            //Employee class does not have createANewEmployee() method
             createANewEmployee("Zazu", "", "Mzingo", "mzi@example.com", "08123456789", "Chyulu Hills", "Full-time", "contract", 40, LocalDate.now(), LocalDate.now().plusMonths(12), true);
         }
     }
@@ -45,7 +41,7 @@ public class DataSeeder implements CommandLineRunner{
         empGenerator.setFinishDate(finishDate);
         empGenerator.setOnGoing(onGoing);
 
-        //you can save in repo under this method too
+
         employeeRepository.save(empGenerator);
         System.out.println("One employee seeded");
     }
