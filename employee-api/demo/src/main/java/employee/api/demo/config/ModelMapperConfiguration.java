1bf96a4c8943898cbeb24622ceee85a0244558de
gischellatte <40295535+gischellatte@users.noreply.github.com>
2026-07-06 02:15:35 +1000

    Update ModelMapperConfiguration.java

--- employee-api/demo/src/main/java/employee/api/demo/config/ModelMapperConfiguration.java
+++ employee-api/demo/src/main/java/employee/api/demo/config/ModelMapperConfiguration.java
@@ -14,10 +14,7 @@ public class ModelMapperConfiguration {
     @Bean
     public ModelMapper modelMapper(){
         ModelMapper modelMap = new ModelMapper();
-         
-        //setSkipNullEnabled(true) if DTO (source) has a null field, ModelMapper will not touch that field in that entity
-        //setPreferNestedProperties(false) means  the mapping works without any exception.
-        //setMatchingStrategy() means the method must be an exact match
+        
         modelMap.getConfiguration()
         .setSkipNullEnabled(true).setPreferNestedProperties(false).setMatchingStrategy(MatchingStrategies.STRICT);
 
