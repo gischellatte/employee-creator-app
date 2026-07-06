643710505634f9871fd84cd9a5652325ca9a1307
gischellatte <40295535+gischellatte@users.noreply.github.com>
2026-07-07 01:46:27 +1000

    Update EmploymentHistoryRepository.java

--- employee-api/demo/src/main/java/employee/api/demo/employmenthistory/repository/EmploymentHistoryRepository.java
+++ employee-api/demo/src/main/java/employee/api/demo/employmenthistory/repository/EmploymentHistoryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import employee.api.demo.employmenthistory.entity.EmploymentHistory;
 
 public interface EmploymentHistoryRepository extends JpaRepository<EmploymentHistory, Integer> {
-    //findAll() is a Spring Data JPA method, findByXxx() must be based on the entity
+
     List <EmploymentHistory> findAll();
 }
