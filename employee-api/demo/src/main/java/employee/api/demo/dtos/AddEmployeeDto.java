92c980709a9ff2bce58f69cc9cba7b9cbb02430b
gischellatte <gismichelle96@gmail.com>
2026-07-13 10:10:03 +1000

    removed redundant extensions

--- employee-api/demo/src/main/java/employee/api/demo/dtos/AddEmployeeDto.java
+++ employee-api/demo/src/main/java/employee/api/demo/dtos/AddEmployeeDto.java
@@ -1,11 +1,6 @@
 package employee.api.demo.dtos;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
